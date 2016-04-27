8c6d2986b21471aeaa8861aac8b48fd6a6b5b1f6
huazhihao <i@huazhihao.com>
2016-04-27 03:29:16 +0000

    Add pysonar v2.1

---
+++ install_jre8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get install oracle-java8-installer oracle-java8-set-default -y
+java -version
