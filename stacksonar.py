becc0996e1682a3f6eeb1adce6917c71ad3e8753
John Hua <john.hua@citrix.com>
2016-04-28 17:53:11 +0800

    Fix src url

--- stacksonar.py
+++ stacksonar.py
@@ -9,6 +9,7 @@ from jinja2 import Environment, FileSystemLoader
 DIR = os.path.dirname(os.path.abspath(__file__))
 INDEX_TEMPLATE = 'index_template.html'
 
+
 def run_pysonar(src_root, html_root="./html"):
     os.system('java -jar pysonar-2.1.jar "%s" "%s"' % (src_root, html_root))
 
@@ -16,14 +17,16 @@ def run_pysonar(src_root, html_root="./html"):
 def gen_log_html(log_path, src_root, html_root="./html"):
     with open(log_path) as log_file:
         lines = log_file.readlines()
-
+    src_dir = os.path.basename(src_root)
     for i in xrange(len(lines)):
         line = lines[i]
         # Trim terminal colors
         line = re.sub(u"\\x1b\[\d{1,3}(;\d{1,3})?m", "", line)
         # TODO: logging_exception_prefix logging_debug_format_suffix
         #       logging_default_format_string logging_context_format_string
-        repl = u'<a href="\g<2>#L\g<3>" target="stack">\g<2>:\g<3></a>'
+        repl = (u'<a href="%s/\g<2>.html#L\g<3>" ',
+                u'target="stack">\g<2>:\g<3></a>') % src_dir
+
         line = re.sub("("+src_root + u")([^:]+\.py):(\d+)", repl, line)
         lines[i] = line
 
@@ -35,7 +38,7 @@ def gen_log_html(log_path, src_root, html_root="./html"):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        "./stacksonar /opt/stack/nova/ /opt/stack/logs/n-cpu.log")
+        "./stacksonar.py /opt/stack/nova/ /opt/stack/logs/n-cpu.log")
     parser.add_argument("src")
     parser.add_argument("log")
 
